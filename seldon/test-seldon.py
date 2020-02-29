31fd662bdb7e8ab9a526191ffe697c5b229ac744
Farshid1 <farshid1@Admins-MacBook-Air.local>
2020-02-29 18:02:24 +0100

    Computer vision by seldon

--- seldon/test-seldon.py
+++ seldon/test-seldon.py
@@ -4,41 +4,15 @@ import cv2
 import requests
 import json
 
-# payload = {"data": {"tensor": {"shape": example_img.shape,
-#                               "values": example_img.reshape(-1).tolist()}}}
-
-# fa = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
 fa = cv2.imread('a.jpg')
 payload = {
     "data": {"tensor": {"shape": fa.shape, "values": fa.reshape(-1).tolist()}}}
-
 response = requests.post('http://localhost:5000/predict', json=payload)
-# far = response.content
-# r_json = response.json()
-# ticket = r_json['ndarray']
-# print(ticket)
-# res = requests.Response
-
 
 data = response.json()['data']
 shape_image = data["tensor"]["shape"]
-
-bb_list = data["tensor"]["values"]
-bb_list = np.array(bb_list, dtype=np.uint8).reshape(
+output_image = data["tensor"]["values"]
+output_image = np.array(output_image, dtype=np.uint8).reshape(
     shape_image[1], shape_image[2])
-cv2.imshow("farshid", bb_list)
+cv2.imshow("farshid", output_image)
 cv2.waitKey()
-
-
-# prediction_list = response.json()['data']['ndarray']
-# prediction_list = far.json()['data']['ndarray']
-# np.array(prediction_list[1], dtype=bool)
-# "data":{"names"
-#
-
-# mask = np.array(r_json, dtype=np.uint8)
-# print(mask)
-# cv2.imshow("farshid", mask)
-# cv2.waitKey(1000)
-# print(response.content)
-# img = img.astype('uint8')
